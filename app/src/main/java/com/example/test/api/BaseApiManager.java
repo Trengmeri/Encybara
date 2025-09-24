0f14c26127a6d8aa4ce797fa0a2ee31dccb73ea2
Trang <merinotran22@gmail.com>
2025-09-24 23:11:53 +0700

    thay server

--- app/src/main/java/com/example/test/api/BaseApiManager.java
+++ app/src/main/java/com/example/test/api/BaseApiManager.java
@@ -6,7 +6,7 @@ import okhttp3.OkHttpClient;
 
 public abstract class BaseApiManager {
 
-    public static final String BASE_URL = "http://14.225.198.3:8080"; // Thay đổi URL của bạn nếu cần
+    public static final String BASE_URL = "http://54.179.180.125:8080"; // Thay đổi URL của bạn nếu cần
     protected static final OkHttpClient client = new OkHttpClient.Builder()
             .connectTimeout(20, TimeUnit.SECONDS) // Thời gian chờ kết nối
             .readTimeout(20, TimeUnit.SECONDS)    // Thời gian chờ đọc dữ liệu
